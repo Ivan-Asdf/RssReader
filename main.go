a8e97513b2741ea2701c49d7ff807f96d4e08d53
Ivan <ivan.asdf0@gmail.com>
2021-09-05 17:23:29 +0300

    Added parsing for Source, SourceUrl & PublishDate.

--- main.go
+++ main.go
@@ -7,7 +7,8 @@ import (
 )
 
 func main() {
-	err := reader.Parse("https://www.rssboard.org/files/sample-rss-092.xml")
+	// err := reader.Parse("https://www.rssboard.org/files/sample-rss-092.xml")
+	err := reader.Parse("https://www.rssboard.org/files/sample-rss-2.xml")
 	if err != nil {
 		fmt.Println(err)
 	}
--- reader/reader.go
+++ reader/reader.go
@@ -6,19 +6,35 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 )
 
-type RssItem struct {
-	Title       string `xml:"title"`
-	Source      string `xml:"source"`
-	SourceURL   string
-	Link        string `xml:"link"`
-	PublishDate string `xml:"pubDate"`
-	Description string `xml:"description"`
+type rawRssItem struct {
+	Title       string     `xml:"title"`
+	Source      itemSource `xml:"source"`
+	Link        string     `xml:"link"`
+	PublishDate string     `xml:"pubDate"`
+	Description string     `xml:"description"`
 }
 
-func getRssItems(xmlReader io.ReadCloser, itemTag string) ([]RssItem, error) {
-	rssItems := make([]RssItem, 0)
+type itemSource struct {
+	XMLName   xml.Name
+	SourceURL string `xml:"url,attr"`
+}
+
+type RssItem struct {
+	Title       string     `json:"title"`
+	Source      string     `json:"source"`
+	SourceUrl   string     `json:"sourceUrl"`
+	Link        string     `json:"link"`
+	PublishDate *time.Time `json:"pubDate"`
+	Description string     `json:"description"`
+}
+
+const ITEM_TAG = "item"
+
+func getRawRssItems(xmlReader io.ReadCloser) ([]rawRssItem, error) {
+	rssItems := make([]rawRssItem, 0)
 
 	decoder := xml.NewDecoder(xmlReader)
 	for {
@@ -31,8 +47,8 @@ func getRssItems(xmlReader io.ReadCloser, itemTag string) ([]RssItem, error) {
 			}
 		}
 		if t, ok := t.(xml.StartElement); ok {
-			if t.Name.Local == itemTag {
-				var item RssItem
+			if t.Name.Local == ITEM_TAG {
+				var item rawRssItem
 				err = decoder.DecodeElement(&item, &t)
 				if err != nil {
 					return nil, err
@@ -43,6 +59,36 @@ func getRssItems(xmlReader io.ReadCloser, itemTag string) ([]RssItem, error) {
 	}
 
 	return rssItems, nil
+}
+
+func getDateFormats() []string {
+	return []string{
+		time.RFC822,
+		"Mon, 02 Jan 2006 15:04:05 GMT",
+	}
+}
+
+func getRssItems(rawItems []rawRssItem) []RssItem {
+	rssItems := make([]RssItem, 0)
+	for _, v := range rawItems {
+		var rssItem RssItem
+		rssItem.Title = v.Title
+		rssItem.Source = v.Source.XMLName.Local
+		rssItem.SourceUrl = v.Source.SourceURL
+		rssItem.Link = v.Link
+
+		for _, format := range getDateFormats() {
+			time, err := time.Parse(format, v.PublishDate)
+			if err == nil {
+				rssItem.PublishDate = &time
+				break
+			}
+		}
+
+		rssItem.Description = v.Description
+		rssItems = append(rssItems, rssItem)
+	}
+	return rssItems
 }
 
 func printJson(object interface{}) {
@@ -56,11 +102,11 @@ func Parse(url string) error {
 		return err
 	}
 
-	rssItems, err := getRssItems(resp.Body, "item")
+	rssItems, err := getRawRssItems(resp.Body)
 	if err != nil {
 		fmt.Println(err)
 	}
-	printJson(rssItems)
+	printJson(getRssItems(rssItems[:2]))
 
 	return nil
 }
