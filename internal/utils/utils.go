606ac5bf2793826fd9622d051a5fec779f164fad
Ivan <ivan.asdf0@gmail.com>
2021-09-06 14:27:02 +0300

    Added internal/utils package.

---
+++ internal/utils/utils.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+
+	"github.com/Ivan-Asdf/RssReader/reader"
+)
+
+func PrintJson(rssItems []reader.RssItem) {
+	jsonString, err := json.MarshalIndent(rssItems, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(jsonString))
+
+	file, err := os.OpenFile("output.json", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bytesWritten, err := file.Write(jsonString)
+	if err != nil {
+		log.Fatal(err)
+	} else if bytesWritten != len(jsonString) {
+		log.Println("Not all bytes written to file")
+	}
+	err = file.Close()
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func GetInput() []string {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatal("Please specify one or more urls as arguments")
+	}
+
+	runTestUrlsFlag := flag.Bool("t", false, "If flag is specified will run use predifined test urls")
+	flag.Parse()
+	if *runTestUrlsFlag {
+		return []string{
+			"https://www.rssboard.org/files/sample-rss-091.xml",
+			"https://www.rssboard.org/files/sample-rss-092.xml",
+			"https://www.rssboard.org/files/sample-rss-2.xml",
+		}
+	} else {
+		return args
+	}
+}
--- main.go
+++ main.go
@@ -1,66 +1,22 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"log"
-	"os"
 
+	"github.com/Ivan-Asdf/RssReader/internal/utils"
 	"github.com/Ivan-Asdf/RssReader/reader"
 )
-
-func printJson(rssItems []reader.RssItem) {
-	jsonString, err := json.MarshalIndent(rssItems, "", "  ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println(string(jsonString))
-
-	file, err := os.OpenFile("output.json", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bytesWritten, err := file.Write(jsonString)
-	if err != nil {
-		log.Fatal(err)
-	} else if bytesWritten != len(jsonString) {
-		log.Println("Not all bytes written to file")
-	}
-	err = file.Close()
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func getInput() []string {
-	args := os.Args[1:]
-	if len(args) == 0 {
-		log.Fatal("Please specify one or more urls as arguments")
-	}
-
-	runTestUrlsFlag := flag.Bool("t", false, "If flag is specified will run use predifined test urls")
-	flag.Parse()
-	if *runTestUrlsFlag {
-		return []string{
-			"https://www.rssboard.org/files/sample-rss-091.xml",
-			"https://www.rssboard.org/files/sample-rss-092.xml",
-			"https://www.rssboard.org/files/sample-rss-2.xml",
-		}
-	} else {
-		return args
-	}
-}
 
 func init() {
 	log.SetFlags(0)
 }
 
 func main() {
-	urls := getInput()
+	urls := utils.GetInput()
 
 	results, errors := reader.Parse(urls)
 	if len(errors) != 0 {
 		log.Println(errors)
 	}
-	printJson(results)
+	utils.PrintJson(results)
 }
